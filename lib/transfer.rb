49d686509c47aacffa86a9ee0a5840d8884ffb6d
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 16:30:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,10 @@ class Transfer
   end
   
   def execute_transaction
-    if sender.balance > amount && status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      status = "complete"
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
       status = "rejected"
       "Transaction rejected. Please check your account balance."
