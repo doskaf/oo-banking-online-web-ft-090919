5a3560514076021a7c4f29cb087977b76308cafd
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 16:30:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      status = "rejected"
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
       
     end
